5807deb4b5ad9d6d76ed86c46bed60167f4ded2c
mrChebur <mrCheburPlugins@inbox.ru>
2023-04-21 00:40:00 +0500

    Stable 2

--- code/gui.py
+++ code/gui.py
@@ -1,6 +1,8 @@
 from pathlib import Path
-from PySide6.QtGui import QPixmap, QMouseEvent
-from PySide6.QtWidgets import QApplication, QMainWindow, QLabel
+from PySide6.QtGui import QPixmap, QMouseEvent, QFont
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QTableWidgetItem, QTableWidget, QVBoxLayout
+from PySide6.QtCore import Qt
+from generate import GenerateTasks
 
 
 class MainWindow(QMainWindow):
@@ -39,10 +41,93 @@ class MainWindow(QMainWindow):
     def showMultiplicationTable(self, event: QMouseEvent):
         print('showMultiplicationTable')
 
+    def open_new_window(self):
+        # Создание нового окна
+        self.new_window = MultiplicationTableWindow()
+        self.new_window.show()
+        self.close()
+
+
+class MultiplicationTableWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.table = QTableWidget(self)
+        # self.table.setShowGrid(False)
+        self.table.verticalHeader().hide()
+        self.table.horizontalHeader().hide()
+        self.table.setFont(QFont('Arial', 15))
+        self.initUI()
+
+    def initUI(self):
+        min_multiplier = 2
+        max_multiplier = 10
+
+        self.setWindowTitle(" ")
+        self.setGeometry(100, 100, 800, 800)
+
+        self.generateTableSize(min_multiplier, max_multiplier, 3)
+        self.fill_table(min_multiplier, max_multiplier)
+
+        # Создание вертикального лейаута
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.table)
+
+        # Установка лейаута
+        self.setLayout(vbox)
+
+    def generateTableSize(self, min_multiplier, max_multiplier, rows_number):
+        column_number = (max_multiplier - min_multiplier + 1) / rows_number
+        remainder = (max_multiplier - min_multiplier + 1) % rows_number
+        if remainder != 0:
+            rows_number += 1
+        self.table.setColumnCount(column_number)
+        self.table.setRowCount(rows_number)
+
+    def fill_table(self, min_multiplier, max_multiplier):
+        column_max, row_max = self.table.columnCount(), self.table.rowCount()
+        multipliers = list(range(min_multiplier, max_multiplier))
+        generate = GenerateTasks()
+        tasks = generate.multiplication(multipliers, shuffle=False)
+        text = ''
+        row = 0
+        column = 0
+        for num, task in enumerate(tasks):
+            text += str(task) + str(task.solve())
+            first_value = str(task).split(' ')[0]
+            try:
+                next_first_value = str(tasks[num + 1]).split(' ')[0]
+            except IndexError:
+                next_first_value = None
+
+            if first_value == next_first_value:
+                text += '\n'
+            else:
+
+                item = QTableWidgetItem(text.replace('*', 'x'))
+                self.table.setItem(row, column, item)
+                text = ''
+                if column < column_max - 1:
+                    column += 1
+                else:
+                    row += 1
+                    column = 0
+
+        self.setAlignment()
+        self.table.resizeRowsToContents()
+        self.table.resizeColumnsToContents()
+
+    def setAlignment(self):
+        for i in range(self.table.rowCount()):
+            for j in range(self.table.columnCount()):
+                item = self.table.item(i, j)
+                if item is not None:
+                    item.setTextAlignment(Qt.AlignLeft | Qt.AlignBottom)
+
 
 if __name__ == '__main__':
     app = QApplication([])
-    window = MainWindow()
+    # window = MainWindow()
+    window = MultiplicationTableWindow()
     window.show()
     app.exec()
 
